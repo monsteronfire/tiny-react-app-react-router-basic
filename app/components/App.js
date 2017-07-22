402d2a209e4dd3c18c4c1ddd1b5128b2d9017d70
Shaii Ong <monsteronfire@gmail.com>
2017-07-22 23:00:54 +0800

    Add Nav

--- app/components/App.js
+++ app/components/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactRouter from 'react-router-dom';
 import { BrowserRouter, Route }  from 'react-router-dom';
+import Nav from './Nav';
+import About from './About';
 
 class App extends React.Component {
   render() {
     return (
-      <Router>
+      <BrowserRouter>
         <div className='main-container'>
+          <Nav />
           <Route path='/about' component={About} />
         </div>
-      </Router>
+      </BrowserRouter>
     )
   }
 }
